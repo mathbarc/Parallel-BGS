b0b8f0a89ff2e50408a46f9c413e07d7a75a1c9b
Alysson Soares <alysson.soares.20@gmail.com>
2016-06-25 21:12:12 -0300

    inicio BGS_cuda

--- BGS.cu
+++ BGS.cu
@@ -1,3 +1,15 @@
 #include "BGS.h"
-
-void BGS(uchar* buffer, int size, uchar* frame, int frame_size);
+__global__
+void BGS(uchar* buffer, int buffer_size, uchar* frame, int frame_size){
+	const int x =  blockIdx.x * blockDim.x + threadIdx.x;
+    const int y =  blockIdx.y * blockDim.y + threadIdx.y;
+    int avg = 0;
+    int sum = 0;
+    if(x < frame_size && y < frame_size){
+    	for(int i = 0; i < buffer_size; i++){
+    		//calcula a soma dos pixels
+     	}
+     	avg = sum / buffer_size;
+     	//calcula a diferença e escreve no frame do vetor resposta
+    }
+}
